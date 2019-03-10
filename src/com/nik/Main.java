27180da890fe8862e558de8d9739954770f669d2
linux <nikhil.thakur46@gmail.com>
2019-03-10 08:52:04 +0530

    first commit

---
+++ src/com/nik/Main.java
@@ -0,0 +1,45 @@
+package com.nik;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int position = calculateHighScorePosition(1500);
+        displayHighScorePosition("Nikhil", position);
+
+        position = calculateHighScorePosition(900);
+        displayHighScorePosition("Adam", position);
+
+        position = calculateHighScorePosition(400);
+        displayHighScorePosition("Sergei", position);
+
+        position = calculateHighScorePosition(50);
+        displayHighScorePosition("Tim", position);
+    }
+
+    //Method to display High Score Position and name
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + " managed to get into position " + position + " on the high score table");
+    }
+
+    //Method to calculate High Score Position according to your score
+    public static int calculateHighScorePosition(int score){
+
+        /*One Way
+        if(score >= 1000)
+            return 1;
+        else if (score >= 500)
+            return 2;
+        else if (score >= 100)
+            return 3;
+
+        return 4;
+        */
+
+        //Other way
+        return (score >= 1000) ? 1 : ((score >= 500) ? 2 : ((score >=100) ? 3 : 4));
+    }
+}
+
+
+
